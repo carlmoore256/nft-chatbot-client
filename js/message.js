0c59fb54460380f6b72982a3429db8fd010020e8
Carl <carlmoore256@gmail.com>
2023-07-10 14:39:03 -0700

    fixed chat scroll; added code block output parsing

--- js/message.js
+++ js/message.js
@@ -12,12 +12,49 @@ export class Message {
         this.message += text;
     }
 
+    formatCode(message) {
+        // Check if the message is a code block
+        const codeBlockPattern = /```([^`]+)```/g;
+        const codeBlockMatch = codeBlockPattern.exec(message);
+        if (codeBlockMatch) {
+            // The code block, including backticks and potential language specification
+            const fullMatch = codeBlockMatch[0];
+            // The content of the code block, without backticks
+            const content = codeBlockMatch[1].trim();
+            // Split the content into lines
+            const lines = content.split('<br>');
+            // The potential language is specified on the first line
+            const potentialLang = lines[0];
+            // Check if the first line is a known language
+            if (['javascript', 'python'].includes(potentialLang)) {
+                // Remove the first line (the language) from the lines array
+                lines.shift();
+                // Join the remaining lines back together
+                const codeWithoutLang = lines.join('<br>');
+                // Replace the matched string with formatted code, adding the language as a class
+                message = message.replace(
+                    fullMatch,
+                    `<div class="code-block language-${potentialLang}"><pre><code>${codeWithoutLang}</code></pre></div>`
+                );
+            } else {
+                // If no known language was found, revert to the previous behavior
+                message = message.replace(
+                    fullMatch,
+                    `<div class="code-block"><pre><code>${content}</code></pre></div>`
+                );
+            }
+        }
+        return message;
+    }
+
     render() {
-        var senderClass = "chat-message-sent";
+        let senderClass = "chat-message-sent";
         if (this.role == "ai") {
             senderClass = "chat-message-received";
         }
-        var formattedMessage = this.message.replace(/\\n/g, '\n').replace(/\n/g, '<br>'); 
+        let formattedMessage = this.message.replace(/\\n/g, '\n').replace(/\n/g, '<br>'); 
+        // try to format code if it is a code block
+        formattedMessage = this.formatCode(formattedMessage);
         return `<div id=${this.id} class="chat-bubble ` + senderClass + '">' + formattedMessage + '</div>';
     }
 
